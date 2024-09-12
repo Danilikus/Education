12a9bf03098cb2bd2d0d9e054730e13317444233
Danilikus <hero1357980@yandex.ru>
2024-09-12 12:02:46 +0300

    12.09.24 С помощью шаблонов создал разные страницы с разным содержимым. Также применил связку шаблонов. Потупин

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -2,14 +2,17 @@ from django.template.response import TemplateResponse
 from django.shortcuts import render
 from django.http import *
 
+# def index(request):
+#     header = "Персональные данные"  # обычная переменная
+#     langs = ["Английский", "Немецкий", "Испанский"]  # массив
+#     user = {"name": "Максим.", "age": 30}  # словарь
+#     addr = ("Виноградная", 23, 45)  # кортеж
+#     data = {"header": header, "langs": langs, "user": user, "address":
+#     addr}
+#     return TemplateResponse(request, "index.html", data)
+
 def index(request):
-    header = "Персональные данные"  # обычная переменная
-    langs = ["Английский", "Немецкий", "Испанский"]  # массив
-    user = {"name": "Максим.", "age": 30}  # словарь
-    addr = ("Виноградная", 23, 45)  # кортеж
-    data = {"header": header, "langs": langs, "user": user, "address":
-    addr}
-    return TemplateResponse(request, "index.html", data)
+    return render(request, "firstapp/index.html")
 
 def about(request):
  return HttpResponse("About")
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from firstapp import views
+from django.views.generic import TemplateView
  
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
+    path('contact/', TemplateView.as_view(template_name="firstapp/contact.html",
+        extra_context={"work": "Разработка программных продуктов"})),
     path('details/', views.details),
 ]
