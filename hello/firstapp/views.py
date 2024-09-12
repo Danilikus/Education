da1f7b1ac86121e75050119c960776cca3524f7b
DemetriusMO <warhammer58913@gmail.com>
2024-09-12 22:48:15 +0300

    12.09.2024 В ходе работы научился использовать специальные теги в HTLM страницах и применил их на практике. Майборода

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -2,17 +2,9 @@ from django.template.response import TemplateResponse
 from django.shortcuts import render
 from django.http import *
 
-# def index(request):
-#     header = "Персональные данные"  # обычная переменная
-#     langs = ["Английский", "Немецкий", "Испанский"]  # массив
-#     user = {"name": "Максим.", "age": 30}  # словарь
-#     addr = ("Виноградная", 23, 45)  # кортеж
-#     data = {"header": header, "langs": langs, "user": user, "address":
-#     addr}
-#     return TemplateResponse(request, "index.html", data)
-
 def index(request):
-    return render(request, "firstapp/index.html")
+ cat = ["Ноутбуки", "Принтеры", "Сканеры", "Диски", "Шнуры"]
+ return render(request, "firstapp/index.html", context={"cat": cat})
 
 def about(request):
  return HttpResponse("About")
