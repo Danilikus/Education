830f858c753c3e044a7ff2994abf91bced8d6c84
Danilikus <hero1357980@yandex.ru>
2024-09-16 23:54:42 +0300

    16.09.24 Научился применять различные специальные поля в формах. Потупин

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -1,26 +1,46 @@
 from django import forms
 
 # class UserForm(forms.Form):
-#  name = forms.CharField(label="Имя клиента")
- # class UserForm(forms.Form):
-#  email = forms.EmailField(label="Электронный адрес", help_text="Обязательный символ - @")
+#     file = forms.ImageField(label="Изображение")
 # class UserForm(forms.Form):
-#  ip_adres = forms.GenericIPAddressField(label="IP адрес", help_text=" Пример формата 192.0.2.0")
+#     date = forms.DateField(label="Введите дату")
 # class UserForm(forms.Form):
-#  reg_text = forms.RegexField(label="Текст", regex="^[0-9][A-F][0-9]$")
+#     time = forms.DateField(label="Введите время")
 # class UserForm(forms.Form):
-#  slug_text = forms.SlugField(label="Введите текст")
+#     date_time = forms.DateTimeField(label="Введите дату и время")
 # class UserForm(forms.Form):
-#  url_text = forms.URLField(label="Введите URL", help_text="Например http://www.google.com")
+#     time_delta = forms.DurationField(label="Введите промежуток времени")
 # class UserForm(forms.Form):
-#  uuid_text = forms.UUIDField(label="Введите UUID", help_text="Формат xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxx")
+#     date_time = forms.SplitDateTimeField(label="Введите дату и время")
 # class UserForm(forms.Form):
-#  combo_text = forms.ComboField(label="Введите URL",
-#  fields=[forms.URLField(), forms.CharField(max_length=20)])
+#     num = forms.IntegerField(label="Введите целое число")
 # class UserForm(forms.Form):
-#  file_path = forms.FilePathField(label="Выберите файл", path="C:/Python37/")
+#     num = forms.DecimalField(label="Введите десятичное число")
 # class UserForm(forms.Form):
-#  file_path = forms.FilePathField(label="Выберите файл", path="C:/Python37/",
-#  allow_files="True", allow_folders="True")
+#     num = forms.DecimalField(label="Введите десятичное число", decimal_places=2)
+# class UserForm(forms.Form):
+#     num = forms.FloatField(label="Введите число")
+# class UserForm(forms.Form):
+#     ling = forms.ChoiceField(label="Выберите язык",
+#         choices=((1, "Английский"),
+#         (2, "Немецкий"),
+#         (3, "Французский")))
+# class UserForm(forms.Form):
+#     city = forms.TypedChoiceField(label="Выберите город",
+#         empty_value=None,
+#         choices=((1, "Москва"),
+#             (2, "Воронеж"),
+#             (3, "Курск")))
+# class UserForm(forms.Form):
+#     country = forms.MultipleChoiceField(label="Выберите страны",
+#         choices=((1, "Англия"),
+#             (2, "Германия"),
+#             (3, "Испания"),
+#             (4, "Россия")))
 class UserForm(forms.Form):
- file = forms.FileField(label="Файл")
+    city = forms.TypedMultipleChoiceField(label="Выберите город",
+        empty_value=None,
+            choices=((1, "Москва"),
+                (2, "Воронеж"),
+                (3, "Курск"),
+                (4, "Томск")))
