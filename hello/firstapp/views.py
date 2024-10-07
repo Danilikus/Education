60d2a36117b327a26f2214443c1471c888aaa40a
DemetriusMO <warhammer58913@gmail.com>
2024-10-07 10:17:58 +0300

    07.10.24 Лекция 10. Ознакомился с моделями данных для работы с базой данных и методами их применения. Майборода

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,55 +1,8 @@
 from django.shortcuts import render
 from django.http import *
 from .forms import UserForm
+from .models import Person
 
-# def index(request):
-#  if request.method == "POST":
-#    userform = UserForm(request.POST)
-#    if userform.is_valid():
-#     name = userform.cleaned_data["name"]
-#     return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
-#    else:
-#     return HttpResponse("Ошибка ввода данных")
-#  else:
-#    userform = UserForm()
-#    return render(request, "firstapp/index.html", {"form": userform})
-
-# def index(request):
-#  userform = UserForm()
-#  return render(request, "firstapp/index.html", {"form": userform})
-
-def index(request):
- userform = UserForm()
- if request.method == "POST":
-   userform = UserForm(request.POST)
-   if userform.is_valid():
-    name = userform.cleaned_data["name"]
-    return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
- return render(request, "firstapp/index.html", {"form": userform})
-
-
-def about(request):
- return HttpResponse("About")
-
-def contact(request):
- return HttpResponseRedirect("/about")
-
-def details(request):
- return HttpResponsePermanentRedirect("/")
-
-
-def m304(request):
-    return HttpResponseNotModified()
-def m400(request):
-    return HttpResponseBadRequest("<h2>Bad Request</h2>")
-def m403(request):
-    return HttpResponseForbidden( "<h2>ForЬidden</h2>")
-def m404(request):
-    return HttpResponseNotFound("<h2>Not Found</h2>")
-def m405(request):
-    return HttpResponseNotAllowed("<h2>Method is not allowed</h2>")
-def m410(request):
-    return HttpResponseGone("<h2>Content is no longer here</h2>")
-
-def m500(request):
-    return HttpResponseServerError("<h2>Something is wrong</h2>")
+klientl = Person.objects.get(narne="Bиктop") 
+klient2 = Person.objects.get(age=25)
+klientЗ = Person.objects.get(name="Bacилий", age=23)
